63bd14f445d2659a270ba8bde9bb6c6c048cedff
Shaista Afreen <97823364+ShaistaAfreen09@users.noreply.github.com>
2022-05-19 19:23:34 +0530

    Update fibnoccai.py

--- fibnoccai.py
+++ fibnoccai.py
@@ -9,12 +9,4 @@ if rev == num:
     print("It is a Palindrome Number")
 else:
     print("It is not a Palindrome Number")
-'''num = int(input("enter a number"))
-if num > 1:
-    for i in range(2,num):
-        if num % i == 0:
-            print("the number is not a prime number")
-            print(i,"times",num//i,"is",num)
-            break
-    else:
-        print(num,"the number is prime number.")'''
+
